b196c264b0f000a8ff06ccd2ee8255495ef10e50
Jussi Pakkanen <jpakkane@gmail.com>
2017-03-14 23:21:33 +0200

    Parse binary package file.

--- debparse.py
+++ debparse.py
@@ -15,6 +15,30 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys, os
+
+class DebParser:
+    def __init__(self):
+        self.packages = {}
+
+    def parse(self, fname):
+        current = {}
+        for line in open(fname):
+            if line == '\n':
+                self.packages[current['Package']] = current
+                current = {}
+                continue
+            if line[0] == ' ':
+                current['Description'] += line.rstrip()
+            else:
+                k, v = line.split(':', 1)
+                current[k] = v.strip()
+
 
 if __name__ == '__main__':
-    print('foob')
+    if len(sys.argv) != 2:
+        print('%d <package file>' % sys.argv[0])
+        sys.exit(0)
+    debparser = DebParser()
+    debparser.parse(sys.argv[1])
+    print(debparser.packages['xwayland'])
