29521bf604d526b640ebf69200def2f74e8f1af1
Jussi Pakkanen <jpakkane@gmail.com>
2017-03-15 19:14:21 +0200

    Parse Ubuntu files and a bit more class usage.

--- debparse.py
+++ debparse.py
@@ -18,9 +18,26 @@
 import sys, os
 import sqlite3
 
+class BinaryPackage:
+    def __init__(self, pdict):
+        self.name = pdict['Package']
+        self.version = pdict['Version']
+        self.description = pdict['Description']
+        self.depends = []
+        for depstr in pdict.get('Depends', '').split(','):
+            for alternates in depstr.split('|'):
+                depsplit = alternates.split('(')
+                if len(depsplit) == 1:
+                    version = None
+                else:
+                    version = depsplit[1].split(''')''')[0]
+                depname = depsplit[0].strip()
+                if depname != '':
+                    self.depends.append((depname, version))
+
 class DebParser:
     def __init__(self):
-        self.packages = {}
+        self.packages = []
         self.dbfile = 'debian.sqlite'
         if not os.path.exists(self.dbfile):
             self.create_db()
@@ -34,41 +51,57 @@ class DebParser:
         c = conn.cursor()
         c.execute('''CREATE TABLE bin_packages(
         name TEXT NOT NULL PRIMARY KEY,
-        version TEXT NOT NULL
+        version TEXT NOT NULL,
+        description TEXT NOT NULL
         );''')
         c.execute('''CREATE TABLE depends(
         name TEXT NOT NULL,
         dependency TEXT NOT NULL,
+        version TEXT,
         FOREIGN KEY(name) REFERENCES bin_packages(name),
         FOREIGN KEY(dependency) REFERENCES bin_packages(name)
         );''')
         conn.commit()
         conn.close()
 
-    def parse(self, fname):
+    def parse_debfile(self, fname):
+        packages = []
         current = {}
+        last_key = None
         for line in open(fname):
             if line == '\n':
-                self.packages[current['Package']] = current
+                packages.append(current)
                 current = {}
                 continue
             if line[0] == ' ':
-                current['Description'] += line.rstrip()
+                current[last_key] += '\n' + line.strip()
             else:
                 k, v = line.split(':', 1)
                 current[k] = v.strip()
+                last_key = k
+        return packages
+
+    def parse(self, fname):
+        for packagedict in self.parse_debfile(fname):
+            bp = BinaryPackage(packagedict)
+            self.packages.append(bp)
 
     def to_db(self):
         c = self.conn.cursor()
-        for name, p in self.packages.items():
-            c.execute('INSERT INTO bin_packages VALUES(?, ?);', (name, p['Version']))
-        self.conn.commit()
-        for name, package in self.packages.items():
-            if 'Depends' not in package:
+        for p in self.packages:
+            try:
+                c.execute('INSERT INTO bin_packages VALUES(?, ?, ?);', (p.name, p.version, p.description))
+            except sqlite3.IntegrityError:
+                # In ubuntu some packages are in the file multiple times.
                 continue
-            for depstr in package['Depends'].split(','):
-                dep = depstr.split('(')[0].strip()
-                c.execute('INSERT INTO depends VALUES(?, ?);', (name, dep))
+        self.conn.commit()
+        for package in self.packages:
+            for dep in package.depends:
+                try:
+                    c.execute('INSERT INTO depends VALUES(?, ?, ?);', (package.name, dep[0], dep[1]))
+                except sqlite3.Error as e:
+                    print('Missing dep', dep[0])
+                    pass
         self.conn.commit()
 
 if __name__ == '__main__':
