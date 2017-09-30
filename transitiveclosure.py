a7331a1e1a01d1677e5b136d96349b1f160c9ea1
Jussi Pakkanen <jpakkane@gmail.com>
2017-10-01 00:07:11 +0300

    Track already processed packages.

--- transitiveclosure.py
+++ transitiveclosure.py
@@ -21,33 +21,23 @@ from dbquery import DbQuery
 def calculate_closure(dbfile, root_package):
     source_packages = set()
     binary_packages = set()
-    src_to_process = set()
     bin_to_process = set()
     bin_to_process.add(root_package)
+    processed_bins = set()
     dbq = DbQuery(dbfile)
 
     round = 0
-    while len(src_to_process) > 0 or len(bin_to_process) > 0:
+    while len(bin_to_process) > 0:
         print('Round', round)
         round += 1
-        print('Source packages:', len(source_packages))
         print('Binary packages:', len(binary_packages))
-        print('Source remaining:', len(src_to_process))
         print('Binary remaining:', len(bin_to_process))
-        if len(src_to_process) > 1000000:
-            s = src_to_process.pop()
-            source_packages.add(s)
-            trials = dbq.binary_packages_of(s)
-            for tarr in trials:
-                t = tarr[0]
-                if t not in binary_packages:
-                    bin_to_process.add(t)
         if len(bin_to_process) > 0:
             p = bin_to_process.pop()
-            #binary_packages.add(p)
+            processed_bins.add(p)
             trials = dbq.reverse_build_deps_of(p)
             for t in trials:
-                if t not in binary_packages:
+                if t not in binary_packages and t not in processed_bins:
                     binary_packages.add(t)
                     bin_to_process.add(t)
     sf = open('sources.txt', 'w')
